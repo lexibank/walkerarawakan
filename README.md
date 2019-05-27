54fb6848f143ae3a74f108a2efc234871dc58d39
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-05-27 12:53:03 +0200

    First commit; orthographic profile missing, languages need review

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # walkerarawakan
 
-Walker and Ribeiro (2011) Arawakan dataset.
-
 Cite the source dataset as
 
-> Robert S. Walker and Lincoln A. Ribeiro. 2011. Bayesian phylogeography of the
-Arawak expansion in lowland South America. *Proceedings of the Royal
-Society B: Biological Sciences*. Volume 278, Number 1718, 2562--2567.
-DOI: 10.1098/rspb.2010.2579
+> Robert S. Walker and Lincoln A. Ribeiro. 2011. Bayesian phylogeography of the Arawak expansion in lowland South America. *Proceedings of the Royal Society B: Biological Sciences*. Volume 278, Number 1718, 2562--2567. DOI: 10.1098/rspb.2010.2579
 
-This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/
-license
+This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/ license
+
+## Statistics
+
+
+
+![Glottolog: 99%](https://img.shields.io/badge/Glottolog-99%25-brightgreen.svg "Glottolog: 99%")
+![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
+![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
+
+- **Varieties:** 57
+- **Concepts:** 100
+- **Lexemes:** 5,725
+- **Synonymy:** 1.13
---
+++ lexibank_walkerarawakan.py
@@ -0,0 +1,86 @@
+# encoding: utf-8
+from __future__ import unicode_literals, print_function
+from collections import OrderedDict, defaultdict
+
+import attr
+from clldutils.misc import slug
+from clldutils.path import Path
+from clldutils.text import split_text, strip_brackets
+from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.dataset import NonSplittingDataset
+from pylexibank.dataset import Concept, Language
+
+from tqdm import tqdm
+
+import csv
+import lingpy
+
+class Dataset(BaseDataset):
+    id = 'walkerarawakan'
+    dir = Path(__file__).parent
+    concept_class = Concept
+    language_class = Language
+
+    def cmd_download(self, **kw):
+        # nothing to do, as the raw data is in the repository
+        pass
+
+    def cmd_install(self, **kw):
+        # read raw lexical data
+        raw_data = self.dir.joinpath('raw', 'Arawakan-lexemes.tsv')
+        with open(raw_data.as_posix()) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter='\t')
+            raw_entries = [row for row in reader]
+
+        # add information to the dataset
+        with self.cldf as ds:
+            # add sources
+            ds.add_sources(*self.raw.read_bib())
+
+            # add languages
+            lang_map = {}
+            for language in self.languages:
+                lid = slug(language['Name'])
+                ds.add_language(
+                    ID=lid,
+                    Name=language['Glottolog_Name'],
+                    Glottocode=language['Glottocode'],
+                )
+                lang_map[language['Glottolog_Name'].strip()] = lid
+
+            # add concepts
+            for concept in self.concepts:
+                ds.add_concept(
+                    ID=concept['ID'],
+                    Name=concept['ENGLISH'],
+                    Concepticon_ID=concept['CONCEPTICON_ID'],
+                    Concepticon_Gloss=concept['CONCEPTICON_GLOSS'],
+                )
+
+            # add lexemes
+            for idx, entry in tqdm(enumerate(raw_entries), desc='make-cldf'):
+                # Add all entries (columns) except `ID`, which holds the
+                # parameter id, and `English`, with a gloss
+                entry.pop('English')
+                pid = entry.pop('ID')
+
+                for lang, value in entry.items():
+                    lang = lang.strip()
+
+                    # skip over problems
+                    if lang in ['Cariai', 'Warekena']:
+                        continue
+
+                    for form in split_text(value):
+                        # tokenize
+                        #segments = [c for c in form]
+                        segments = []
+
+                        # add form
+                        for row in ds.add_lexemes(
+                            Language_ID=lang_map[lang],
+                            Parameter_ID=pid,
+                            Value=form,
+                            Segments=segments,
+                            Source=['walker2011']):
+                            pass
